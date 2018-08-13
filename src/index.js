0df3a7f6ac780aa0f89037444256448be1706ab0
Clay Branch <clay.branch@gmail.com>
2018-08-13 12:11:35 -0500

    added functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-export default () => {
-  console.log('test')
+import createGenericContext from 'react-cosmos-test/generic'
+import {render as baseRender} from 'react-testing-library'
+
+const createTestContext = fixture => {
+  const context = createGenericContext({
+    fixture,
+    renderer: baseRender,
+  })
+  return context
+}
+
+export const render = fixture => {
+  const {mount, getWrapper} = createTestContext(fixture)
+  mount()
+  return getWrapper()
 }
